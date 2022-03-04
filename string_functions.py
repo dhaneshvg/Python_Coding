919456279b9693a8f117851e4e8548f74d5fd666
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:29:51 +0530

    String Functions

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+# newstring = "Hello World"
+# print(newstring.endswith("World"))
+# print(newstring.startswith("Hello"))
+# print(newstring.upper())
+# print(newstring.lower())
+
+print(min(1, 2, 0, 5, 6, 8))
+print(max(5, 11, 1, 42, 8, 9, 4))
+print(abs(-55))
