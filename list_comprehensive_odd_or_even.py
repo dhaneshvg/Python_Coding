64dad25b665eadc081c64bc6d9d7258784347c13
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:27:37 +0530

    List Comprehensive odd or even

---
+++ list_comprehensive_odd_or_even.py
@@ -0,0 +1,8 @@
+# even = [i for i in range(10) if i % 2 == 0]
+# odd = [i for i in range(10) if i % 2 != 0]
+# print(odd)
+# print(even)
+
+list = [2, 3, 4, 5, 6, 7]
+even = [i*i for i in list if i % 2 == 0]
+print(even)
