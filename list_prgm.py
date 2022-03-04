34a2327219f36f57b5b1fb98859674c477b3de62
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:12:18 +0530

    LIST OPERATIONS
    List operations in Python

---
+++ list_prgm.py
@@ -0,0 +1,37 @@
+# list1 = ["a", "b", "c", 1, 2, 3, 4]
+# list2 = ["x", "y", "z", 4, 5, 6]
+# print(list1)
+
+# print the 5th element in the list 1
+# print(list1[5])
+
+# concatenation of two list list1 and list2
+# print(list1 + list2)
+
+# list3 = ["apple", "orange", "grapes", "pineapple"]
+# # to find the grapes is in the list3
+# print("grapes" in list3)
+#
+# # to insert plum in list3 in the position 2 (index value 2)
+# list3[2] = "plum"
+# print(list3)
+
+#########    List Functions    #########
+
+##########    append add the element in last ##############
+# list4 = ["elephant", "tiger", "dog", "giraffe"]
+# list4.append("lion")
+# print(list4)
+
+##########   insert the element in the position in the index ##########
+# list5 =["elephant", "lion","giraffe", "dog"]
+# list5.insert(2,"tiger")
+# print(list5)
+
+#########  Length #############
+# list6 = ["car", "bus", "bike", "auto rickshaw", "aeroplane"]
+# print(len(list6))
+
+######## index value ############
+list7 = ["car", "aeroplane", "auto rickshaw", "bus"]
+print(list7.index("bus"))
