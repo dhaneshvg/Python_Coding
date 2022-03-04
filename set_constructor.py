d9b0733a6507db79859f42690e5ff7e6043e7dd4
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:41:08 +0530

    set constructor

---
+++ set_constructor.py
@@ -0,0 +1,32 @@
+# set1 = set(("aa", "bb", "cc"))
+# print(set1)
+
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3, 4}
+# set3 = set1.union(set2)
+# print(set3)
+
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3, 4}
+# set3 = set1 | set2
+# print(set3)
+
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3, 4}
+# set1.update(set2)
+# print(set1)
+
+# set1 = {1,2,3,4}
+# set2 = {2,3,4,5}
+# set3 = set1-set2
+# set4 = set2-set1
+# print(set3)
+# print(set4)
+
+# lis= [1,2,2,3,4,4,1,2,3]
+# print(set(lis))
+
+lis = {1, 2, 2, 3, 4, 5, 1, 7, 7}
+t = sorted(lis)
+print(t)
+print(t[::-1])
