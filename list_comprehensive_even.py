8e9ecec65d11a013542e33e59c6cdeba87ba14d8
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:27:17 +0530

    List Comprehensive even

---
+++ list_comprehensive_even.py
@@ -0,0 +1,10 @@
+# even = []
+# for i in range(10):
+#     if i % 2 == 0:
+#         even.append(i)
+# print(even)
+
+# list comprehensive
+
+even = [i for i in range(20) if i % 2 == 0]
+print(even)
