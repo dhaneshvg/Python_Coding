345ed79acb434cd01581d004024d94e812e2e361
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:16:31 +0530

    FUNCTION RETURN VALUE
    MAKING FUNCTION RETURN VALUE IN PYTHON

---
+++ fun_return.py
@@ -0,0 +1,19 @@
+# def sum1(a, b):
+#     print(a + b)
+#
+#
+# x = int(input("Enter the first number:"))
+# y = int(input("Enter the second number:"))
+# sum1(x, y)
+
+# function with return value
+def sum2(a, b):
+    # c = a + b
+    # return c
+    return a+b
+
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number"))
+print(sum2(x, y))
+print(sum2(10, 20))
