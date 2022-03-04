bf8836a2ece16748e178f3d1da01e493e93aa60b
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:13:23 +0530

    BOOLEAN LOGIC IN PYTHON
    Boolean Logic

---
+++ boolean_logic.py
@@ -0,0 +1,13 @@
+# username = "admin"
+# password = 123
+# if username == "amdin" and password == 123:
+#     print("access guaranteed")
+# else:
+#     print("access denied")
+
+username = "admin"
+password = 123
+if username == "admin" or password == 23:
+    print("access guaranteed")
+else:
+    print("access denied")
