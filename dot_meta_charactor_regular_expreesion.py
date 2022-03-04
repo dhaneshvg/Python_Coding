bfb34e277a6d5f1d845dee687c94fe38869999f1
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:43:40 +0530

    dot meta charactor regular expreesion

---
+++ dot_meta_charactor_regular_expreesion.py
@@ -0,0 +1,19 @@
+import re
+
+# pattern = r"dha.esh"
+# if re.match(pattern, "dhanesh"):
+#     print("Matched")
+# else:
+#     print("Not matched")
+
+# pattern = r"dha.es."
+# if re.match(pattern, "dhanesh"):
+#     print("Matched")
+# else:
+#     print("Not matched")
+
+pattern = r"dha.es."
+if re.match(pattern, "dhannesh"):
+    print("Matched")
+else:
+    print("Not matched")
