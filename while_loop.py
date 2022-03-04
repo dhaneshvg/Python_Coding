7580a169a4f3c4d4694a6eea0e2510a14ed09bce
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:14:13 +0530

    WHILE LOOP IN PYTHON

---
+++ while_loop.py
@@ -0,0 +1,26 @@
+# a = 5
+# while a < 10:
+#     print(a)
+#     a += 1
+
+# count = 0
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+#  Break and continue
+
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+
+j = 1
+while j < 7:
+    print(j)
+    if j == 3:
+        break
+    j += 1
