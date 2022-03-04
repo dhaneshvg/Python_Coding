a9de6c80218f59ed9a970e5c08c2f4c415cdf687
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:26:57 +0530

    list Comprehensive

---
+++ list_comprehensive.py
@@ -0,0 +1,17 @@
+# def square():
+#     for i in range(10):
+#         print(i * i)
+#
+#
+# square()
+
+li = [i * i for i in range(10)]
+print(li)
+
+# lettres = []
+# for i in "avodha":
+#     lettres.append(i)
+# print(lettres)
+
+letters = [i for i in "avodha"]
+print(letters)
