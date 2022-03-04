412aff220a133f620af68f00357032212eddb2d2
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:39:52 +0530

    Set in python
    A Set is an unordered collection data type that is iterable, mutable and has no duplicate elements.
    Sets are used to store multiple items in a single variable.

---
+++ set.py
@@ -0,0 +1,30 @@
+# set1 = {"a", "b", "c", "d"}
+# print(set1)
+
+# set2 = {"a", "b", "c", "d"}
+# for i in set2:
+#     print(i)
+
+# set3 = {"a", "b", "c", "d"}
+# print("b" in set3)
+# print("f" in set3)
+
+# set4 = {"a","b","c","d"}
+# set4.add("e")
+# print(set4)
+# print(len(set4))
+# set4.update(["f","g","h","i"])
+# print(set4)
+# print(len(set4))
+
+# set5 = {"a", "b", "c", "d", "e"}
+# set5.remove("b")
+# print(set5)
+# set5.discard("c")
+# print(set5)
+
+set6 = {"a", "b", "c", "d", "e"}
+set6.pop()
+print(set6)
+set6.clear()
+print(set6)
