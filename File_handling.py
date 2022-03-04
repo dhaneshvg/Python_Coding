57098104353c5d5a17b9f2233b26e5cc136f13f9
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:20:39 +0530

    File Handling

---
+++ File_handling.py
@@ -0,0 +1,21 @@
+# file1 = open("demo.txt", "w")
+# file1.close()
+
+# file1 = open("demo.txt", "r")
+# content = file1.read()
+# print(content)
+# file1.close()
+
+# file1 = open("abcd.txt", "w")
+# file1.write("Hai...Dhanesh")
+# file1.write("How are you?")
+# file1.close()
+
+# file1 = open("abcd.txt", "a")
+# file1.write("Iam Fine")
+# file1.close()
+
+fl1 = open("xyz.txt", 'r')
+a = fl1.read()
+print(a)
+fl1.close()
