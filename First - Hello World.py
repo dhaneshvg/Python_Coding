13f1423c17787912bab2aedfc59031869371d285
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:04:51 +0530

    Python - First
    Python Basic Coding

---
+++ First - Hello World.py
@@ -0,0 +1,56 @@
+# print('hello world')
+#
+# # addition
+# a = 10
+# b = 20
+# type(a)
+# print(a)
+# print(a)
+# c = a + b
+# print(c)
+#
+# # multiplication
+# print(10 * 20)
+# # Division
+# print(100 / 20)  # with float
+# print(100 // 20)  # with out float
+# # Reminder
+# print(7 % 3)
+
+# # type
+# a = 10
+# print(type(a))
+# b = "10"
+# print(type(b))
+
+# # Concatenation
+# a = "20"
+# b = "45"
+# c = a + b
+# print(c)
+#
+# # Square and rise to the power
+# a = 5
+# b = a**5
+# print(b)
+#
+# c = 2
+# d = c*3
+# print(d)
+#
+# # Square root
+# a = 49
+# b = a**(1/2)
+#
+# # operator overloading
+# print("Hello"*5)
+#
+# print("Hello 'Dhanesh' Welcome to python")
+# print('Hello "Dhanesh" Welcome to python')
+# print("Hello \"Dhanesh\" Welcome to python")
+
+# place operator
+a = 1
+a = a + 1
+a += 1
+print(a)
