60f49dd03e392d0ba40137dc4613b0a483c63496
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:07:59 +0530

    Python - if & elif
    Python if and elif condition

---
+++ If_progm.py
@@ -0,0 +1,18 @@
+# # if
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("You can vote!")
+# else:
+#     print("Sorry you are not eligible.")
+
+# elif
+
+mark = int(input("Enter your mark:"))
+if mark >= 90:
+    print("A grade")
+elif mark >= 80:
+    print("B grade")
+elif mark >= 70:
+    print("C grade")
+else:
+    print("failed")
