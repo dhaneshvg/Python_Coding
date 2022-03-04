4f4c93c42b0174a1b5532d3c4073f6636386191e
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:42:46 +0530

    Find and Replace - Regular Expression

---
+++ find_and_replace_regular_expression.py
@@ -0,0 +1,17 @@
+import re
+
+# str = "Hai dhanesh How are you?"
+# pattern = r"dhanesh"
+# new1 = re.sub(pattern, "Dhanesh Villwamangalath",str)
+# print(new1)
+
+# pattern =r"dhanesh"
+# new1 = re.sub(pattern,"Dhanesh Villwamangalath", "Hai dhanesh How are you?")
+# print(new1)
+
+# new1 = re.sub("dhanesh","Dhanesh Villwamangalath","Hai dhanesh How are you?")
+# print(new1)
+
+# print(re.sub("dhanesh","Dhanesh Villwamangalath", "Hai dhanesh How are you?"))
+
+
