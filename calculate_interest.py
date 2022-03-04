e6c6eafa9e6db8b1ac96e288e2c32fc74d9080e0
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-04 19:48:18 +0530

    calculate interest

---
+++ calculate_interest.py
@@ -0,0 +1,7 @@
+# This program is used to calculate interest
+a = int(input('Enter the principal Amount (P):'))
+b = int(input('Enter the number of years (N):'))
+c = int(input('Enter the rate of interest (R):'))
+i = (a * b * c)/100
+print('Interest (I) = (PxNxR)/100')
+print(i)
